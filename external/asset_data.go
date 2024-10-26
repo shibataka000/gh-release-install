7de6d3d15363f97b3e9b25b84ef77c8c28e58213
Takao Shibata <chise.alter.pasta@gmail.com>
2024-10-26 22:50:06 +0900

    save

--- external/asset_data.go
+++ external/asset_data.go
@@ -1,7 +1,40 @@
 package external
 
 var defaultAssetTemplates = map[string]AssetTemplateList{
+	"gravitational/teleport": {},
 	"hashicorp/terraform": {
-		mustNewAssetTemplateFromString(""),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_darwin_amd64.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_darwin_arm64.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_freebsd_386.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_freebsd_amd64.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_freebsd_arm.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_linux_386.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_linux_amd64.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_linux_arm.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_linux_arm64.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_openbsd_386.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_openbsd_amd64.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_solaris_amd64.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_windows_386.zip"),
+		mustNewAssetTemplateFromString("https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_windows_amd64.zip"),
+	},
+	"helm/helm": {
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-darwin-amd64.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-darwin-arm64.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-linux-386.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-linux-amd64.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-linux-arm.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-linux-arm64.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-linux-ppc64le.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-linux-riscv64.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-linux-s390x.tar.gz"),
+		mustNewAssetTemplateFromString("https://get.helm.sh/helm-v3.15.2-windows-amd64.zip"),
+	},
+	"kubernetes/kubernetes": {
+		mustNewAssetTemplateFromString("https://dl.k8s.io/release/v1.30.2/bin/darwin/amd64/kubectl"),
+		mustNewAssetTemplateFromString("https://dl.k8s.io/release/v1.30.2/bin/darwin/arm64/kubectl"),
+		mustNewAssetTemplateFromString("https://dl.k8s.io/release/v1.30.2/bin/linux/amd64/kubectl"),
+		mustNewAssetTemplateFromString("https://dl.k8s.io/release/v1.30.2/bin/linux/arm64/kubectl"),
+		mustNewAssetTemplateFromString("https://dl.k8s.io/release/v1.30.2/bin/windows/amd64/kubectl.exe"),
 	},
 }
