c9e5fb0c931651a300a48b18e04b499963203ac4
Takao Shibata <chise.alter.pasta@gmail.com>
2024-10-27 10:51:54 +0900

    save

---
+++ external/asset_test.go
@@ -0,0 +1,33 @@
+package external
+
+import (
+	"testing"
+)
+
+func TestAssetTemplateExecute(_ *testing.T) {
+	// todo: implement this.
+}
+
+func TestAssetTemplateListExecute(_ *testing.T) {
+	// todo: implement this.
+}
+
+func TestAssetTemplateMapHas(_ *testing.T) {
+	// todo: implement this.
+}
+
+func TestAssetTemplateMapGet(_ *testing.T) {
+	// todo: implement this.
+}
+
+func TestAssetRepositoryHas(_ *testing.T) {
+	// todo: implement this.
+}
+
+func TestAssetRepositoryList(_ *testing.T) {
+	// todo: implement this.
+}
+
+func TestAssetRepositoryDownload(_ *testing.T) {
+	// todo: implement this.
+}
