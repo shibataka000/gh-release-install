78b942d3acfee916aa62156f2fbc9b54a23fafa0
Takao Shibata <chise.alter.pasta@gmail.com>
2024-12-14 11:45:09 +0900

    chore: print error message to stderr instead of stdout

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/shibataka000/gh-release-install/cmd"
 )
@@ -9,6 +10,7 @@ import (
 func main() {
 	log.SetFlags(0)
 	if err := cmd.NewCommand().Execute(); err != nil {
+		log.SetOutput(os.Stderr)
 		log.Fatal("Error: ", err)
 	}
 }
