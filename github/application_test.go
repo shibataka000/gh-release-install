352ad4bf2d99bda7f4075a87076115e8a1e41e97
Takao Shibata <chise.alter.pasta@gmail.com>
2024-12-14 22:07:01 +0900

    refactor: delete unnecessary functions

--- github/application_test.go
+++ github/application_test.go
@@ -12,22 +12,10 @@ func TestApplicationServiceInstall(_ *testing.T) {
 	// todo: implement this.
 }
 
-func TestApplicationServiceFindAssetAndPattern(_ *testing.T) {
-	// todo: implement this.
-}
-
-func TestApplicationServiceFindExecBinary(_ *testing.T) {
-	// todo: implement this.
-}
-
 func TestApplicationServiceListAssets(_ *testing.T) {
 	// todo: implement this.
 }
 
 func TestApplicationServiceDonwload(_ *testing.T) {
-	// todo: implement this.
-}
-
-func TestApplicationServiceExtract(_ *testing.T) {
 	// todo: implement this.
 }
