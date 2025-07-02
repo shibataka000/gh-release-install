e31293bfe8de05210bb957c9b511f7f465ef19db
Takao Shibata <chise.alter.pasta@gmail.com>
2025-07-02 23:46:44 +0900

    w

--- linux_amd64_test.go
+++ linux_amd64_test.go
@@ -359,20 +359,3 @@ func TestApplicationServiceForLinuxAmd64(t *testing.T) {
 		})
 	}
 }
-
-// must is a helper that wraps a call to a function returning (E, error) and panics if the error is non-nil.
-// This is intended for use in variable initializations.
-func must[E any](e E, err error) E {
-	if err != nil {
-		panic(err)
-	}
-	return e
-}
-
-// clone [exec.Cmd] and return it.
-func clone(t *testing.T, cmd *exec.Cmd) *exec.Cmd {
-	t.Helper()
-	newCmd := exec.Command(cmd.Args[0], cmd.Args[1:]...)
-	newCmd.Dir = cmd.Dir
-	return newCmd
-}
