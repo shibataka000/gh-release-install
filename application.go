4f7f4585ba7c56ccbc41e7c8d397c3651d742071
Takao Shibata <chise.alter.pasta@gmail.com>
2025-07-02 22:20:07 +0900

    w

--- application.go
+++ application.go
@@ -26,57 +26,45 @@ type AssetRepository interface {
 }
 
 // Find a GitHub release asset in given release which matches given patterns and returns it and an executable binary in it.
-func (app *ApplicationService) Find(ctx context.Context, tag string, patterns map[string]string) (FindResult, error) {
-	release := newRelease(tag)
+func (app *ApplicationService) Find(ctx context.Context, tag string, patterns map[string]string) (Asset, ExecBinary, error) {
+	release := Release{
+		tag: tag,
+	}
 
 	ps, err := parsePatternMap(patterns)
 	if err != nil {
-		return FindResult{}, err
+		return Asset{}, ExecBinary{}, err
 	}
 
 	assets, err := app.asset.list(ctx, release)
 	if err != nil {
-		return FindResult{}, err
+		return Asset{}, ExecBinary{}, err
 	}
 
 	asset, pattern, err := findAssetAndPattern(assets, ps)
 	if err != nil {
-		return FindResult{}, err
+		return Asset{}, ExecBinary{}, err
 	}
 
 	execBinary, err := pattern.execute(asset)
 	if err != nil {
-		return FindResult{}, err
+		return Asset{}, ExecBinary{}, err
 	}
 
-	return newFindResult(asset, execBinary), nil
+	return asset, execBinary, nil
 }
 
 // Install downloads a GitHub release asset, extracts an executable binary from it, and writes it into given directory.
-func (app *ApplicationService) Install(ctx context.Context, result FindResult, dir string) error {
-	assetContent, err := app.asset.download(ctx, result.Asset)
+func (app *ApplicationService) Install(ctx context.Context, asset Asset, execBinary ExecBinary, dir string) error {
+	assetContent, err := app.asset.download(ctx, asset)
 	if err != nil {
 		return err
 	}
 
-	execBinaryContent, err := assetContent.extract(result.ExecBinary)
+	execBinaryContent, err := assetContent.extract(execBinary)
 	if err != nil {
 		return err
 	}
 
-	return app.execBinary.write(result.ExecBinary, execBinaryContent, dir)
-}
-
-// FindResult represents the result of [ApplicationService.Find].
-type FindResult struct {
-	Asset      Asset
-	ExecBinary ExecBinary
-}
-
-// newFindResult returns a new [FindResult] object.
-func newFindResult(asset Asset, execBinary ExecBinary) FindResult {
-	return FindResult{
-		Asset:      asset,
-		ExecBinary: execBinary,
-	}
+	return app.execBinary.write(execBinary, execBinaryContent, dir)
 }
